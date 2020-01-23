ca154cd0dddffcb69e230056ea8a5458baf4a7af
Stanislav Kuchinskii <ksastan@gmail.com>
2020-01-23 22:19:22 +0300

    add script and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Gitlab Jobs
+Script get all running and pending jobs from Gitlab api
+
+#### Execute
+```
+python3 main.py <url> <token>
+```
---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import argparse
+
+global parser
+
+
+def get_parameters():
+    global parser
+    parser = argparse.ArgumentParser(description='Enter --url and --token')
+    parser.add_argument('url', help='Gitlab api URL')
+    parser.add_argument('token', help='Gitlab user token id')
+
+
+def gitlab_jobs_status(tokenid, gitlab_url):
+    print("Executing...")
+    projects = requests.get(
+        '%s/api/v4/projects?per_page=100' % gitlab_url,
+        headers={'Private-Token': tokenid})
+
+    projects_list = json.loads(projects.text)
+
+    running_jobs = 0
+    pending_jobs = 0
+    runners = {}
+    # get all projects
+    for project in projects_list:
+        jobs = requests.get(
+            '%s/api/v4/projects/%s/jobs?scope[]=pending&scope[]=running&per_page=100' % (gitlab_url, project['id']),
+            headers={'Private-Token': tokenid})
+        jobs_list = json.loads(jobs.text)
+        # get all jobs in each project
+        for job in jobs_list:
+            print("Project: %s" % (project['path_with_namespace']))
+            print("  Pipeline: %s" % (job['status']))
+            if job['status'] == 'pending':
+                pending_jobs += 1
+            elif job['status'] == 'running':
+                running_jobs += 1
+                print("  Runner name: %s" % (job['runner']['description']))
+                try:
+                    runners[job['runner']['description']]
+                except KeyError:
+                    runners = {job['runner']['description']: 1}
+                else:
+                    runners = {job['runner']['description']: runners.get(job['runner']['description'])+1}
+            else:
+                print('Please check project status')
+    print(80*'-')
+    for runner in runners:
+        print("Runner %s has %s jobs" % (runner, runners[runner]))
+    print(80*'-')
+    print('Summary:')
+    print('  Running jobs: %s' % running_jobs)
+    print('  Pending jobs: %s' % pending_jobs)
+    return running_jobs, pending_jobs
+
+
+if __name__ == '__main__':
+    get_parameters()
+    args = parser.parse_args()
+    gitlab_jobs_status(args.token, args.url)
